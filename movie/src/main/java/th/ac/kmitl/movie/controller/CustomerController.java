26faee41de9331eca4bf7691a25c724d60ec7060
61050818 Kedwadee Wata <70447420+KedwadeeWata@users.noreply.github.com>
2020-11-30 00:20:07 +0700

    Completed

---
+++ movie/src/main/java/th/ac/kmitl/movie/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package th.ac.kmitl.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.kmitl.movie.Model.Customer;
+import th.ac.kmitl.movie.service.CustomerService;
+
+
+@Controller
+@RequestMapping("/Register")
+public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public String getCustomerPage(Model model) {
+
+
+        model.addAttribute("allCustomers", customerService.getCustomers());
+        return "Register";
+    }
+    @PostMapping
+    public String registerCustomer(@ModelAttribute Customer customer, Model model) {
+        customerService.createCustomer(customer);
+        model.addAttribute("allCustomers", customerService.getCustomers());
+        return "redirect:Register";
+    }
+}
---
+++ movie/src/main/java/th/ac/kmitl/movie/controller/MovieActionController.java
@@ -0,0 +1,15 @@
+package th.ac.kmitl.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MovieActionController {
+    @RequestMapping("/Action")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Sawaddee");
+        // return home.html
+        return "Action";
+    }
+}
